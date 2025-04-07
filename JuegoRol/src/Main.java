dc385e081b42baf84c3a4736ceea379fce56d669
juan david <jdacevedo.24@est.ucab.edu.ve>
2025-04-07 09:53:36 -0400

    main

--- JuegoRol/src/Main.java
+++ JuegoRol/src/Main.java
@@ -1,3 +1,4 @@
+import Personajes.Clerigo;
 import Personajes.Mago;
 import Personajes.Personaje;
 
@@ -5,17 +6,35 @@ public class Main {
     public static void main(String[] args) {
         Mago mago = new Mago("juan", "humano", 10, 20, 100, 100, new String[]{"", "", "", ""});
 
+        Mago mago2 = new Mago("pablo", "orco", 10, 20, 100, 100, new String[]{"", "", "", ""});
+
         Personaje objetivo = new Personaje("Pedro", "humano", 10, 20, 100, 100);
 
-        mago.imprimir();
+        Clerigo personaje = new Clerigo("pepe", "humano", 19, 15, 100, 100, "omniman");
 
         mago.AprenderHechizo("helado");
+        mago.AprenderHechizo("fuegardo");
 
-        mago.lanzaHechizo(objetivo);
-
-        objetivo.imprimir();
+        mago2.AprenderHechizo("helado");
 
         mago.imprimir();
+        mago2.imprimir();
+
+        mago.lanzaHechizo(mago2);
+        mago2.lanzaHechizo(mago);
+
+        personaje.curar(mago2);
+
+        mago.lanzaHechizo(mago2);
+
+        personaje.imprimir();
+        mago.imprimir();
+        mago2.imprimir();
+
+
+
+
+
 
     }
 }
